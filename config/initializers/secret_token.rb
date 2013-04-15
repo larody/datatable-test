ab1d15d59eed02cd97db61256c50abcdabb6248a
larody <throwbases@gmail.com>
2013-04-16 06:44:01 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatatableTest::Application.config.secret_token = '1055fa5959361868c310fd6f0c725f60ed9a01070634d1b765683debb7f418b1f21d8791215e27af3492fcab5938511b9e6b1c414ff052896a9e0294efcf68c0'
